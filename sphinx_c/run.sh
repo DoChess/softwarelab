8ef5c98102e95787b8f4253d6ca9664bda1ef19b
Matheus Miranda <matheusmirandalacerda@gmail.com>
2018-04-18 17:25:59 -0300

    Add basic grammar
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- sphinx_c/run.sh
+++ sphinx_c/run.sh
@@ -1,3 +1,3 @@
 gcc -o testing_mic testing_mic.c -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" `pkg-config --cflags --libs pocketsphinx sphinxbase`
 
-./testing_mic -inmic yes -dict model/cmudict-en-us.dict -hmm model/cmusphinx-en-us-8khz-5.2
+./testing_mic -inmic yes -dict model/cmudict-en-us.dict -jsgf grammar/chess_grammar.jsgf -hmm model/cmusphinx-en-us-8khz-5.2
